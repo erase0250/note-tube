6280030dffa19d5e720da3a7532997866e888a81
Jiwoo <erase0250@gmail.com>
2025-05-22 23:32:20 +0900

    feat: 채널 정보 관련 API 함수 추가

--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -11,4 +11,28 @@ export default class YoutubeApi {
     async videos(params) {
         return this.httpClient.get("videos", params);
     }
+
+    async channels(params) {
+        return this.httpClient.get("channels", params);
+    }
+
+    async channelImageURL(channelId) {
+        const res = await this.channels({
+            params: {
+                part: "snippet",
+                id: channelId,
+            },
+        });
+        return res.data.items[0]?.snippet?.thumbnails?.default?.url; ///////
+    }
+
+    async getChannelInfo(channelId) {
+        const res = await this.channels({
+            params: {
+                part: "snippet,statistics",
+                id: channelId,
+            },
+        });
+        return res.data.items[0];
+    }
 }
