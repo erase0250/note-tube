570c058406f9e85ddeb1a349366096c93c7aca69
Jiwoo <erase0250@gmail.com>
2025-07-08 17:31:59 +0900

    fix: Vite 환경을 위해 PostCSS 설정 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+    // Vite에서는 plugins를 객체만 인식해서 배열을 객체 형태로 수정
+    plugins: {
+        "@tailwindcss/postcss": {},
+    },
 };
 
 export default config;
